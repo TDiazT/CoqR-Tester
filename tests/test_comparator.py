351e7e2e06afc93a17ba4717c9286b36dd796f90
Tomas <toumasaldia@gmail.com>
2017-11-22 13:32:15 -0300

    Add more cases for not implemented

--- tests/test_comparator.py
+++ tests/test_comparator.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from Comparator.Comparator import Comparator
-from Comparator.Constants import SUCCESSFUL, CASE_NOT_IMPLEMENTED, CASE_ERROR
+from Comparator.Constants import SUCCESSFUL, CASE_NOT_IMPLEMENTED, CASE_ERROR, CASE_ASSIGNMENT
 
 
 class TestComparator(TestCase):
@@ -9,5 +9,10 @@ class TestComparator(TestCase):
         self.comparator = Comparator()
 
     def test_case_not_implemented(self):
-        result = self.comparator.compare_multiple([CASE_NOT_IMPLEMENTED], [CASE_ERROR])
-        self.assertEqual(result, [CASE_NOT_IMPLEMENTED])
+        self.assertEqual(self.comparator.compare_multiple([CASE_NOT_IMPLEMENTED], [CASE_ERROR]), [CASE_NOT_IMPLEMENTED])
+        self.assertEqual(self.comparator.compare_multiple([CASE_NOT_IMPLEMENTED], [CASE_ASSIGNMENT]),
+                         [CASE_NOT_IMPLEMENTED])
+        self.assertEqual(self.comparator.compare_multiple([CASE_NOT_IMPLEMENTED], [['[1]', "TRUE"]]),
+                         [CASE_NOT_IMPLEMENTED])
+        self.assertEqual(self.comparator.compare_multiple([CASE_NOT_IMPLEMENTED], [['[1]', '1', '[2]', '4']]),
+                         [CASE_NOT_IMPLEMENTED])
