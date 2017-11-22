50755c0e42a50a4cd34a1bf6e35e38274bab44c3
Tomas <toumasaldia@gmail.com>
2017-11-22 13:30:13 -0300

    Fix missing list call and forgot zipping joins in tuples
    I was returning the map object instead of converting it to list.
    
    Also, I forgot that zip gave a tuple, so function should use only
    one parameter

--- Comparator/Comparator.py
+++ Comparator/Comparator.py
@@ -6,21 +6,21 @@ class Comparator:
     def compare_multiple(self, coq_outputs, r_outputs):
         assert(len(coq_outputs) == len(r_outputs))
 
-        return map(self.__compare, zip(coq_outputs, r_outputs))
+        return list(map(self.__compare, zip(coq_outputs, r_outputs)))
 
     @staticmethod
-    def __compare(coq_output, r_output):
-        if coq_output == CASE_NOT_IMPLEMENTED:
+    def __compare(outputs):
+        if outputs[0] == CASE_NOT_IMPLEMENTED:
             return CASE_NOT_IMPLEMENTED
-        elif coq_output == CASE_IMPOSSIBLE:
+        elif outputs[0] == CASE_IMPOSSIBLE:
             return CASE_IMPOSSIBLE
-        elif coq_output == CASE_ERROR:
-            if r_output == CASE_ERROR:
+        elif outputs[0] == CASE_ERROR:
+            if outputs[1] == CASE_ERROR:
                 return SUCCESSFUL
             else:
                 return CASE_ERROR
         else:
-            if r_output == CASE_ASSIGNMENT:
+            if outputs[1] == CASE_ASSIGNMENT:
                 return SUCCESSFUL
             else:
-                return SUCCESSFUL if coq_output == r_output else NOT_EQUAL
+                return SUCCESSFUL if outputs[0] == outputs[1] else NOT_EQUAL
--- tests/test_comparator.py
+++ tests/test_comparator.py
@@ -1,11 +1,13 @@
 from unittest import TestCase
 
 from Comparator.Comparator import Comparator
-from Comparator.Constants import SUCCESSFUL
+from Comparator.Constants import SUCCESSFUL, CASE_NOT_IMPLEMENTED, CASE_ERROR
 
 
 class TestComparator(TestCase):
     def setUp(self):
         self.comparator = Comparator()
 
-
+    def test_case_not_implemented(self):
+        result = self.comparator.compare_multiple([CASE_NOT_IMPLEMENTED], [CASE_ERROR])
+        self.assertEqual(result, [CASE_NOT_IMPLEMENTED])
