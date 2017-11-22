3176a2bccc31928141495a91ff80e7b8027c7e59
Tomas <toumasaldia@gmail.com>
2017-11-22 11:13:49 -0300

    Refactor extract run subprocess to private method

--- Comparator/CoqRunner.py
+++ Comparator/CoqRunner.py
@@ -5,13 +5,13 @@ class CoqRunner:
     proveR = ["make", "run", "-C", "/Users/Tomas/Documents/Memoria/Coq-R/proveR/"]
 
     def run_expression(self, expression):
-        p1 = Popen(["echo", expression], stdout=PIPE)
-        p2 = Popen(self.proveR, stdin=p1.stdout, stdout=PIPE, stderr=STDOUT, universal_newlines=True)
-        p1.stdout.close()
-        return p2.communicate()[0]
+        return self.__run(["echo", expression])
 
     def run_file(self, filename):
-        p1 = Popen(["cat", filename], stdout=PIPE)
+        return self.__run(["cat", filename])
+
+    def __run(self, cmd):
+        p1 = Popen(cmd, stdout=PIPE)
         p2 = Popen(self.proveR, stdin=p1.stdout, stdout=PIPE, stderr=STDOUT, universal_newlines=True)
         p1.stdout.close()
         return p2.communicate()[0]
