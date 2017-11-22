2dc6eda3e284d61b58a3cf29a544eb1030f98a72
Tomas <toumasaldia@gmail.com>
2017-11-22 12:49:33 -0300

    Add R output processor class with tests

---
+++ Comparator/ROutputProcessor.py
@@ -0,0 +1,66 @@
+import re
+
+ERROR = "Error:"
+
+NOT = "Not"
+
+IMPOSSIBLE = "Impossible"
+
+SEQ_TOKEN = "SPECIAL_SUPER_TOKEN"
+
+NULL = "NULL"
+
+CASE_NOT_IMPLEMENTED = "CASE_NOT_IMPLEMENTED"
+
+CASE_ERROR = "CASE_ERROR"
+
+CASE_IMPOSSIBLE = "CASE_IMPOSSIBLE"
+
+CASE_ASSIGNMENT = "CASE_ASSIGNMENT"
+
+
+class ROutputProcessor:
+    vec_res_regex = re.compile('\[\d+\]')
+    error_regex = re.compile('Error:*')
+    flag = False
+
+    def process(self, output):
+        result = []
+        splitlines = [row.split() for row in output.splitlines()]
+        if not output:
+            result.append(CASE_ASSIGNMENT)
+            return result
+
+        for line in splitlines:
+            for word in line:
+                if self.error_regex.match(word):
+                    result.append(CASE_ERROR)
+                    self.flag = False
+                    break
+                elif self.vec_res_regex.match(word) is not None:
+                    if self.vec_res_regex.match(word).group() == '[1]':
+                        if line[1] == '"%s"' % SEQ_TOKEN:
+                            if not self.flag:
+                                self.flag = True
+                            else:
+                                result.append(CASE_ASSIGNMENT)
+                        else:
+                            result.append(line)
+                            self.flag = False
+                    else:
+                        result[-1].extend(line)
+                        self.flag = False
+                    break
+                elif word == NULL:
+                    result.append(NULL)
+                    break
+                elif word == SEQ_TOKEN:
+                    if not self.flag:
+                        self.flag = True
+                    else:
+                        result.append(CASE_ASSIGNMENT)
+                    break
+                else:
+                    break
+
+        return result
