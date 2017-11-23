866f8c8eb96aedcb1e578208e032111fc7905101
Tomas <toumasaldia@gmail.com>
2017-11-23 01:58:42 -0300

    Add first attempt at handling tokens and invisible

--- Comparator/Comparator.py
+++ Comparator/Comparator.py
@@ -1,12 +1,42 @@
 from Comparator.Constants import CASE_NOT_IMPLEMENTED, CASE_ERROR, CASE_IMPOSSIBLE, SUCCESSFUL, CASE_INVISIBLE, \
-    NOT_EQUAL
+    NOT_EQUAL, SEQ_TOKEN
 
 
 class Comparator:
     def compare_multiple(self, coq_outputs, r_outputs):
-        assert(len(coq_outputs) == len(r_outputs))
+        i = j = 0
+        token_flag = False
+        results = []
 
-        return list(map(self.__compare, zip(coq_outputs, r_outputs)))
+        while i < len(coq_outputs) and j < len(r_outputs):
+            if coq_outputs[i] == CASE_NOT_IMPLEMENTED:
+                results.append(CASE_NOT_IMPLEMENTED)
+            elif coq_outputs[i] == CASE_IMPOSSIBLE:
+                results.append(CASE_IMPOSSIBLE)
+            elif coq_outputs[i] == CASE_ERROR:
+                if r_outputs[j] == CASE_ERROR:
+                    results.append(SUCCESSFUL)
+                else:
+                    results.append(NOT_EQUAL)
+            elif coq_outputs[i] == SEQ_TOKEN:
+                if r_outputs[j] == SEQ_TOKEN:
+                    token_flag = True
+                else:
+                    results.append(NOT_EQUAL)
+            else:
+                if r_outputs[j] == SEQ_TOKEN:
+                    if token_flag:
+                        results.append(SUCCESSFUL)
+                        i += 1
+                    else:
+                        results.append(NOT_EQUAL)
+                else:
+                    results.append(SUCCESSFUL if coq_outputs[i] == r_outputs[j] else NOT_EQUAL)
+
+            i += 1
+            j += 1
+
+        return results
 
     @staticmethod
     def __compare(outputs):
--- tests/test_comparator.py
+++ tests/test_comparator.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from Comparator.Comparator import Comparator
-from Comparator.Constants import SUCCESSFUL, CASE_NOT_IMPLEMENTED, CASE_ERROR, CASE_INVISIBLE
+from Comparator.Constants import SUCCESSFUL, CASE_NOT_IMPLEMENTED, CASE_ERROR, CASE_INVISIBLE, SEQ_TOKEN
 
 
 class TestComparator(TestCase):
@@ -16,3 +16,11 @@ class TestComparator(TestCase):
                          [CASE_NOT_IMPLEMENTED])
         self.assertEqual(self.comparator.compare_multiple([CASE_NOT_IMPLEMENTED], [['[1]', '1', '[2]', '4']]),
                          [CASE_NOT_IMPLEMENTED])
+
+    def test_simple_token(self):
+        self.assertEqual(self.comparator.compare_multiple([SEQ_TOKEN], [SEQ_TOKEN]), [])
+
+    def test_assignment_with_tokens(self):
+        coq = [SEQ_TOKEN, ['[1]', '1'], SEQ_TOKEN]
+        r = [SEQ_TOKEN, SEQ_TOKEN]
+        self.assertEqual(self.comparator.compare_multiple(coq, r), [SUCCESSFUL])
