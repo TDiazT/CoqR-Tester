9e217bf4cdd33456c176f6ff6aff9a2679cd2fb1
Tomas <toumasaldia@gmail.com>
2017-12-01 15:48:54 -0300

    Fix missing r interpreter from environment variables

--- scripts/runner.py
+++ scripts/runner.py
@@ -17,7 +17,8 @@ lines = [line.strip() for line in lines]
 for i, line in enumerate(filter(None, lines)):
     exec_time = time.time()
     if os.environ.get('RSCRIPT'):
-        out = subprocess.run(['rscript', '-e', line], universal_newlines=True, stdout=subprocess.PIPE,
+        r_interp = os.environ.get('RSCRIPT')
+        out = subprocess.run([r_interp, '-e', line], universal_newlines=True, stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT).stdout
         interp = 'R'
     elif os.environ.get('COQ_INTERP'):
