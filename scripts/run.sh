2804cf7d08b99715491d4a609eacc4ec576d75b3
Tomas <toumasaldia@gmail.com>
2017-12-01 15:45:11 -0300

    Add creating a directory for output and checking for file to read

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,50 +4,54 @@
 if [ -n "$1" ]
 then
 
-    parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-    cd "$parent_path"
-    cd ..
-
-    SRC=$1
-    OUT=out
-
-    ROUT=r.json
-    COQOUT=coq.json
-    SCRIPTS=scripts
-
-
-    source "$SCRIPTS/settings.sh"
-
-
-
-    if [ -z $2 ]
+    if [ -f $1 ]
     then
+        parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+        SRC=$1
+        OUT=cr_out
+
         ROUT=r.json
-    else
-        ROUT=$2
-    fi
-
-    if [ -z $3 ]
-    then
         COQOUT=coq.json
+        SCRIPTS=$parent_path
+
+        echo "Creating a directory named $OUT for output"
+        mkdir -p $OUT
+
+        source "$SCRIPTS/settings.sh"
+
+        if [ -z $2 ]
+        then
+            ROUT=r.json
+        else
+            ROUT=$2
+        fi
+
+        if [ -z $3 ]
+        then
+            COQOUT=coq.json
+        else
+            COQOUT=$3
+        fi
+
+        echo "Running R interpreter"
+        RSCRIPT=$RSCRIPT python $SCRIPTS/runner.py R $SRC "$OUT/$ROUT"
+
+        echo "Running Coq interpreter"
+        COQ_INTERP=$COQ_INTERP python $SCRIPTS/runner.py asdf $SRC $OUT/$COQOUT
+
+        echo "Cleaning outputs"
+        python $SCRIPTS/cleaner.py $OUT/$ROUT $OUT/clean-$ROUT
+        python $SCRIPTS/cleaner.py $OUT/$COQOUT $OUT/clean-$COQOUT
+
+        echo "Comparing"
+        python $SCRIPTS/comparator.py $OUT/clean-$COQOUT $OUT/clean-$ROUT $OUT/comparison.json
+
+        echo "Done"
     else
-        COQOUT=$3
+        echo "File not found"
     fi
 
-    echo "Running R interpreter"
-    RSCRIPT=$RSCRIPT python $SCRIPTS/runner.py R $SRC "$OUT/$ROUT"
-
-    echo "Running Coq interpreter"
-    COQ_INTERP=$COQ_INTERP python $SCRIPTS/runner.py asdf $SRC $OUT/$COQOUT
-
-    echo "Cleaning outputs"
-    python $SCRIPTS/cleaner.py $OUT/$ROUT $OUT/clean-$ROUT
-    python $SCRIPTS/cleaner.py $OUT/$COQOUT $OUT/clean-$COQOUT
-
-    echo "Comparing"
-    python $SCRIPTS/comparator.py $OUT/clean-$COQOUT $OUT/clean-$ROUT $OUT/comparison.json
-
-    echo "Done"
 else
     echo "Missing R source file to run"
 fi
