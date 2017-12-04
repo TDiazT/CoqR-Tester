02e281c89318e2e955aed4d22e7731be96c8e709
Tomas <toumasaldia@gmail.com>
2017-12-04 11:44:40 -0300

    Add argparse to cleaner script

--- rcoq/cleaner.py
+++ rcoq/cleaner.py
@@ -2,17 +2,27 @@ import argparse
 import json
 import sys
 
+from rcoq.processors.CoqOutputProcessor import CoqOutputProcessor
 from rcoq.processors.ROutputProcessor import ROutputProcessor
 
-input_ = sys.argv[1]
+parser = argparse.ArgumentParser(description='Processes output and returns a new standard one')
 
-parser = argparse.ArgumentParser(description="Processes output and returns a new standard one")
+# #
+parser.add_argument('interp')
+parser.add_argument('input-file')
+parser.add_argument('output-file')
 
-parser.add_argument("interp", )
-# parser.add_argument('--Coq')
-# parser.add_argument('--R')
+options = parser.parse_args()
 
-processor = ROutputProcessor()
+if options.interp == 'R':
+    processor = ROutputProcessor()
+elif options.interp == 'Coq':
+    processor = CoqOutputProcessor()
+else:
+    sys.exit('"%s" is not a valid interpreter (either "R" or "Coq")' % options.interp)
+
+input_ = options['input-file']
+output_ = options['output-file']
 
 with open(input_) as file_:
     outputs = json.load(file_)
@@ -21,5 +31,5 @@ with open(input_) as file_:
         result = processor.process(output['output'])
         output['clean_output'] = result
 
-with open(sys.argv[2], "w") as file_:
+with open(sys.argv[2], 'w') as file_:
     json.dump(outputs, file_, indent=2)
