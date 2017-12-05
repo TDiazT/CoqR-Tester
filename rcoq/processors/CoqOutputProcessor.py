88c64bd10368717635c5133150c09f4f8e9efb70
Tomas <toumasaldia@gmail.com>
2017-12-04 22:15:59 -0300

    Change CoqOutputProcessor to handle single output strings

--- rcoq/processors/CoqOutputProcessor.py
+++ rcoq/processors/CoqOutputProcessor.py
@@ -1,43 +1,35 @@
 import re
 
 from rcoq.Constants import ERROR, NOT, IMPOSSIBLE, SEQ_TOKEN, NULL, CASE_NOT_IMPLEMENTED, CASE_ERROR, \
-    CASE_IMPOSSIBLE
+    CASE_IMPOSSIBLE, CASE_INVISIBLE, CASE_FUNCTION, CASE_UNKNOWN
 
 
 class CoqOutputProcessor:
-    vec_res_regex = re.compile('\[\d+\]')
+    vec_res_regex = re.compile(r'\[\d+\][ \w-]+')
+    error_regex = re.compile(r'Error:*')
+    null_regex = re.compile(r'NULL')
+    function_regex = re.compile(r'(closure)')
+    not_implemented = re.compile(r'Not implemented')
+    impossible = re.compile(r'Impossible')
 
     def process(self, output):
-        result = []
-
-        splitlines = [line.split() for line in output.splitlines()]
-
-        for line in splitlines:
-            for word in line:
-                if word == ERROR:
-                    result.append(CASE_ERROR)
-                    break
-                elif word == NOT:
-                    result.append(CASE_NOT_IMPLEMENTED)
-                    break
-                elif word == IMPOSSIBLE:
-                    result.append(CASE_IMPOSSIBLE)
-                    break
-                elif self.vec_res_regex.match(word) is not None:
-                    if self.vec_res_regex.match(word).group() == '[1]':
-                        if line[1] == '"%s"' % SEQ_TOKEN:
-                            result.append(SEQ_TOKEN)
-                        else:
-                            result.append(line)
-                    else:
-                        result[-1].extend(line)
-                    break
-                elif word == '(%s)' % NULL:
-                    result.append(NULL)
-                    break
-                elif word == '>':
-                    continue
-                else:
-                    break
+        if not output:
+            result = CASE_INVISIBLE
+        elif self.not_implemented.search(output):
+            result = CASE_NOT_IMPLEMENTED
+        elif self.impossible.search(output):
+            result = CASE_IMPOSSIBLE
+        elif self.error_regex.search(output):
+            result = CASE_ERROR
+        elif re.search(self.vec_res_regex, output):
+            matches = self.vec_res_regex.findall(output)
+            result = " ".join(matches)
+        elif self.function_regex.search(output):
+            result = CASE_FUNCTION
+        elif self.null_regex.search(output):
+            result = NULL
+        else:
+            result = CASE_UNKNOWN
 
         return result
+
--- tests/test_coqOutputProcessor.py
+++ tests/test_coqOutputProcessor.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from rcoq.Constants import NULL, CASE_ERROR, SEQ_TOKEN
+from rcoq.Constants import NULL, CASE_ERROR, CASE_FUNCTION, CASE_INVISIBLE, CASE_UNKNOWN
 from rcoq.processors.CoqOutputProcessor import CoqOutputProcessor
 
 
@@ -9,57 +9,61 @@ class TestCoqOutputProcessor(TestCase):
         self.processor = CoqOutputProcessor()
 
     def test_process_2(self):
-        result = self.processor.process("[1] 2\n")
-        self.assertEqual(result, [['[1]', '2']])
+        result = self.processor.process("Success.\n[1] 2\n")
+        self.assertEqual(result, '[1] 2')
 
     def test_process_TRUE(self):
-        result = self.processor.process("[1] TRUE\n")
-        self.assertEqual(result, [['[1]', 'TRUE']])
+        result = self.processor.process("Success.\n[1] TRUE\n")
+        self.assertEqual(result, '[1] TRUE')
 
     def test_process_FALSE(self):
-        result = self.processor.process("[1] FALSE\n")
-        self.assertEqual(result, [['[1]', 'FALSE']])
+        result = self.processor.process("Success.\n[1] FALSE\n")
+        self.assertEqual(result, '[1] FALSE')
 
     def test_process_NA(self):
-        result = self.processor.process("[1] NA\n")
-        self.assertEqual(result, [['[1]', 'NA']])
+        result = self.processor.process("Success.\n[1] NA\n")
+        self.assertEqual(result, '[1] NA')
 
     def test_process_NaN(self):
-        result = self.processor.process("[1] NaN\n")
-        self.assertEqual(result, [['[1]', 'NaN']])
+        result = self.processor.process("Success.\n[1] NaN\n")
+        self.assertEqual(result, '[1] NaN')
+
+    def test_process_Inf(self):
+        result = self.processor.process("[1] Inf\n")
+        self.assertEqual(result, "[1] Inf")
+        result = self.processor.process("[1] -Inf\n")
+        self.assertEqual(result, "[1] -Inf")
 
     def test_process_NULL(self):
-        result = self.processor.process("(NULL)\n")
-        self.assertEqual(result, [NULL])
+        result = self.processor.process("Success.\nNULL\n")
+        self.assertEqual(result, NULL)
 
     def test_process_error_object(self):
         result = self.processor.process("> Error: [eval] Object not found.\nAn error lead to an undefined result.\n")
-        self.assertEqual(result, [CASE_ERROR])
+        self.assertEqual(result, CASE_ERROR)
 
     def test_process_error_function(self):
         result = self.processor.process(
             "> Error: [findFun3] Could not find function “e”.\nAn error lead to an undefined result.\n")
-        self.assertEqual(result, [CASE_ERROR])
+        self.assertEqual(result, CASE_ERROR)
 
     def test_vector_output(self):
-        result = self.processor.process("[1] 1 2 3\n[4] 5 6 7\n")
-        self.assertEqual(result, [['[1]', '1', '2', '3', '[4]', '5', '6', '7']])
+        result = self.processor.process("Success.\n[1] 1 2 3\n[4] 5 6 7\n")
+        self.assertEqual(result, '[1] 1 2 3 [4] 5 6 7')
 
-    def test_single_token(self):
-        result = self.processor.process("[1] \"%s\"" % SEQ_TOKEN)
-        self.assertEqual(result, [SEQ_TOKEN])
 
-    def test_multiple_tokens(self):
-        result = self.processor.process("[1] \"%s\"\n[1] \"%s\"\n[1] \"%s\"" % (SEQ_TOKEN, SEQ_TOKEN, SEQ_TOKEN))
-        self.assertEqual(result, [SEQ_TOKEN, SEQ_TOKEN, SEQ_TOKEN])
+    def test_assignment_with_empty_array(self):
+        result = self.processor.process("")
+        self.assertEqual(result, CASE_INVISIBLE)
 
-    def test_multiple_outputs(self):
-        output = "[1] \"%s\"\n(%s)\n%s\n%s\n%s\n[1] \"%s\"\n[1] \"%s\"\n%s" % (SEQ_TOKEN, NULL, "[1] 1 2 3",
-                                                                               "Error: [findFun3] Could not find function “e”.",
-                                                                               "An error lead to an undefined result.",
-                                                                               SEQ_TOKEN,
-                                                                               SEQ_TOKEN,
-                                                                               "[1] TRUE")
-        result = self.processor.process(output)
-        self.assertEqual(result,
-                         [SEQ_TOKEN, NULL, ['[1]', '1', '2', '3'], CASE_ERROR, SEQ_TOKEN, SEQ_TOKEN, ['[1]', 'TRUE']])
+    def test_function(self):
+        result = self.processor.process("Success.\n(closure)\n")
+        self.assertEqual(result, CASE_FUNCTION)
+
+    def test_unknown(self):
+        result = self.processor.process("anything")
+        self.assertEqual(result, CASE_UNKNOWN)
+        result = self.processor.process("adfasd")
+        self.assertEqual(result, CASE_UNKNOWN)
+        result = self.processor.process("[,1]")
+        self.assertEqual(result, CASE_UNKNOWN)
