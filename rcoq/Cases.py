ee51bbf4599e8333df819607540416b1630ae594
Tomas <toumasaldia@gmail.com>
2017-12-05 17:48:55 -0300

    Add Cases Enum

---
+++ rcoq/Cases.py
@@ -0,0 +1,26 @@
+from aenum import Enum, unique
+
+
+# https://stackoverflow.com/a/43863085/3802589
+@unique
+class Cases(Enum):
+    _init_ = 'value string'
+
+    NOT_IMPLEMENTED = 1, "Not implemented"
+    ERROR = 2, "Error"
+    IMPOSSIBLE = 3, "Impossible"
+    INVISIBLE = 4, "Invisible"
+    SUCCESSFUL = 5, "Successful"
+    UNSUCCESSFUL = 6, "Unsuccesful"
+    FUNCTION = 7, "Function"
+    UNKNOWN = 8, "Unknown"
+
+    def __str__(self):
+        return self.string
+
+    @classmethod
+    def _missing_value_(cls, value):
+        for member in cls:
+            if member.string == value:
+                return member
+
