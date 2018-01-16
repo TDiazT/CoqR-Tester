6e73b99cfbad92411d6e6d76be0bfae4dbda2dd9
Tomas <toumasaldia@gmail.com>
2018-01-16 13:37:20 -0300

    Remove... Report class?

--- Comparator/Report.py
+++
@@ -1,8 +0,0 @@
-class Report:
-    def __init__(self, coq_output, r_output, status_code, line_number) -> None:
-        self.coq_output = coq_output
-        self.r_output = r_output
-        self.status_code = status_code
-        self.line_number = line_number
-
-
