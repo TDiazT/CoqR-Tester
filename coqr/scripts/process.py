5f70f4a3b82d83123286aed243b1de4941e50059
Tomas <toumasaldia@gmail.com>
2018-01-19 16:37:27 -0300

    Refactor process script a bit

--- coqr/scripts/process.py
+++ coqr/scripts/process.py
@@ -1,5 +1,8 @@
 import argparse
+import json
 import sys
+
+import time
 
 from coqr.processors.CoqOutputProcessor import CoqOutputProcessor
 from coqr.processors.ROutputProcessor import ROutputProcessor
@@ -7,17 +10,9 @@ from coqr.utils.file import read_json_file, write_to_file
 
 parser = argparse.ArgumentParser(description='Processes output and returns a new standard one')
 
-# #
 parser.add_argument('interp')
 parser.add_argument('input')
-parser.add_argument('output')
-
-
-def process_file(input_, output_, processor):
-    previous_reports = read_json_file(input_)
-    new_reports = processor.process_reports(previous_reports)
-
-    write_to_file(output_, new_reports)
+parser.add_argument('-output')
 
 
 if __name__ == '__main__':
@@ -30,4 +25,16 @@ if __name__ == '__main__':
     else:
         sys.exit('"%s" is not a valid interpreter (either "R" or "Coq")' % options.interp)
 
-    process_file(options.input, options.output, processor)
+    print("Processing file %s" % options.input)
+    current_time = time.time()
+    previous_reports = read_json_file(options.input)
+    new_reports = processor.process_reports(previous_reports)
+
+    print("Done in %f seconds" % (time.time() - current_time))
+    if options.output:
+        print('Results may be found in %s' % options.output)
+        write_to_file(options.output, new_reports)
+
+    else:
+        print("-------------------------------")
+        print(json.dumps(new_reports, indent=2))
