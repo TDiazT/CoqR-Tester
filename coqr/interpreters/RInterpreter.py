07b46216cc9bb1d055a0224fa5f953871c303553
Tomas <toumasaldia@gmail.com>
2018-04-22 15:29:28 -0300

    Refactor cmd used to run R interpreter

--- coqr/interpreters/RInterpreter.py
+++ coqr/interpreters/RInterpreter.py
@@ -11,10 +11,11 @@ class RInterpreter(AbstractInterpreter):
     def __init__(self, interp) -> None:
         super().__init__(interp)
         self.name = "R"
+        self.cmd = [self.interpreter, '--slave', '--save', '--restore']
 
     def interpret(self, expression):
         p1 = subprocess.Popen(['echo', expression], stdout=subprocess.PIPE)
-        p2 = subprocess.Popen([self.interpreter, '--slave', '--save', '--restore'], stdin=p1.stdout,
+        p2 = subprocess.Popen(self.cmd, stdin=p1.stdout,
                               stdout=subprocess.PIPE,
                               stderr=subprocess.STDOUT, universal_newlines=True)
         p1.stdout.close()
